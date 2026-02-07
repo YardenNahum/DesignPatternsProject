5385111879e4fa5f2df2a10158dcdcb75356d39c
YardenNahum <yardennahum2@gmail.com>
2026-02-07 15:48:20 +0200

    InsuranceManagment App - HomePage GUI

---
+++ src/Backend/HomePage/InsuranceType.java
@@ -0,0 +1,24 @@
+package Backend.HomePage;
+
+public enum InsuranceType {
+    APARTMENT,
+    CAR,
+    HEALTH,
+    LIFE;
+
+    @Override
+    public String toString() {
+        switch (this){
+            case InsuranceType.CAR:
+                return "Car Insurance";
+            case LIFE:
+                return "Life Insurance";
+            case HEALTH:
+                return "Health Insurance";
+            case APARTMENT:
+                return "APARTMENT Insurance";
+            default:
+                return "Not Found";
+        }
+    }
+}
---
+++ src/Launcher.java
@@ -0,0 +1,7 @@
+import HomePage.HomePageController;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HomePage.HomePageController.class, args);    }
+}
