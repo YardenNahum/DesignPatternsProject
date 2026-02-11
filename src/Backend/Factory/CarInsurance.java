cb2d0004d7d7e674aa96b936167777b632e9ef49
mymonganem11 <mymonganem11@gmail.com>
2026-02-11 17:34:47 +0200

    deleted Factory files

--- src/Backend/Factory/CarInsurance.java
+++
@@ -1,9 +0,0 @@
-package Backend.Factory;
-
-// Car insurance implementation
-public class CarInsurance implements Insurance {
-    @Override
-    public void purchaseInsurance() {
-        System.out.println("Car insurance");
-    }
-}
